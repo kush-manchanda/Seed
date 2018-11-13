80d2ffca1893de4690f82b18f76aeca65a23b6c0
Kush <kushm66@gmail.com>
2018-11-14 04:16:41 +0530

    Dashboard v1

--- Dash/app.py
+++ Dash/app.py
@@ -1,32 +1,33 @@
-# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+from si7021 import get_tempC,get_humidity
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+from datetime import datetime
+import pytz
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+location_india=pytz.timezone('Asia/Kolkata')
+time_india=datetime.now(location_india)
+timestamp='{:%H:%M:%S}'.format(time_india)
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [7, 7, -3], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
+app =dash.Dash()
+
+app.layout=html.Div(children=[
+	html.H1('Dash'),
+	dcc.Graph(id='Values',
+		figure ={
+			'data': [
+			{'x':timestamp,'y':get_tempC,'type':'line', 'name':"Temperature"},
+			{'x':timestamp,'y':get_humidity,'type':'line', 'name':"Humidity"},
+			],
+			'layout':{
+			'title':PFC values
+			}
+		}) 
+	])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+	app.run_server(debug=True)
+	
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ A minimalistic code based on MVP - Open Agriculture, using python running on ras
 With cron as scheduler to call python files which includes 3 actuators ( LED , Exhaust Fan and Circulation Fan) ,as per the temperature and humidity from si7021 sensor using the thermostat file.
 
 A Dashboard based on Dash by Plotly (open source ) is used for GUI of data and images from the 2 webcams.
+Working:
+Cron is the scheduler and call the thermostat file which maintains target temperature (Seed/Python/buildVariable.py) by switching on/off the Exhaust Fan
+It also switches the Lights on / off at the specified time (Seed/scripts/Seed_cron.txt)
